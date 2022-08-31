28c9cf373efea14199ac28b270b71b4bdd0fee8f
Sarah Collison <sarah@blameless.com>
2022-08-31 07:56:12 -0700

    Customization

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 
 .PHONY: merge-upstream
 merge-upstream:
+	#mv SD_ROOT/wz_mini/wz_mini.conf SD_ROOT/wz_mini/wz_mini.conf.merge
 	git fetch upstream
 	git merge upstream/master
 
--- SD_ROOT/wz_mini/usr/share/scripts/startup.sh
+++ SD_ROOT/wz_mini/usr/share/scripts/startup.sh
@@ -2,13 +2,9 @@
 
 sleep 15
 
-[ -z "$GITHUB_AUTHORIZED_KEYS" ] || { mkdir -m 700 /root/.ssh; curl -L https://github.com/$GITHUB_AUTHORIZED_KEYS.keys >> /root/.ssh/authorized_keys; }
-
 if [ ! -f /tmp/hosts ]; then
-  echo "127.0.0.1 localhost" > /tmp/hosts
+  echo "127.0.0.1 localhost" > /opt/wz_mini/tmp/.storage/hosts
 fi
-mount --bind /tmp/hosts /etc/hosts
+mount --bind /opt/wz_mini/tmp/.storage/hosts /etc/hosts
 
 /opt/wz_mini/usr/share/scripts/snapshot /opt/wz_mini/www/snapshot.jpg &
-
-/opt/wz_mini/tmp/.bin/crond -b -c /opt/wz_mini/etc/crontab/
